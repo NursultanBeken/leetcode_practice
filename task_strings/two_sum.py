0a1596b51bdb96a1ab52ac191f7ae5cf32f71b3b
Nursultan Bekenov <nursultan.bekenov@t1a.com>
2020-04-26 18:46:15 -0400

    add task two sum

---
+++ task_strings/two_sum.py
@@ -0,0 +1,15 @@
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        map={}
+        result_list=[]
+
+        for i in range(len(nums)):
+            map[nums[i]]=i
+
+        for i in range(len(nums)):
+            comp = target - nums[i]
+
+            if (comp in map) and (map[comp]!=i):
+                result_list.extend( (i,map[comp]))
+                return result_list
+        
