686ef1ba0a079725a2c52c1a9d2b53148b67fda4
Nursultan Bekenov <nur.beken.usa@gmail.com>
2020-08-21 16:41:15 -0400

    last prep

--- FB/commom_words.py
+++ FB/commom_words.py
@@ -71,6 +71,25 @@ class Solution:
         return  max_length   
 
 
+class WordDictionary:
+    def __init__(self):
+        self.d = defaultdict(set)
+        
+
+    def addWord(self, word: str) -> None:
+        self.d[len(word)].add(word)
+        
+
+    def search(self, word: str) -> bool:
+        m = len(word)
+        for dict_word in self.d[m]:
+            i = 0
+            while i < m and (dict_word[i] == word[i] or word[i] == '.'):
+                i += 1
+            if i == m:
+                return True
+        return False
+
 if __name__=="__main__":
     print(findCommon("This is document  tick  tick one one", "I get doc one is"))
     result = UncommonWords("Firstly this is the first string", "Next is the second string")
