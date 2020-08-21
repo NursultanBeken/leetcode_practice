fbbe9c90cac0bb721767f69a430a398f4b97f86d
Nursultan Bekenov <nur.beken.usa@gmail.com>
2020-08-21 16:44:30 -0400

    last prep

--- FB/commom_words.py
+++ FB/commom_words.py
@@ -90,6 +90,33 @@ class WordDictionary:
                 return True
         return False
 
+
+class WordDictionary(object):
+    def __init__(self):
+        self.word_dict = collections.defaultdict(list)
+        
+
+    def addWord(self, word):
+        if word:
+            self.word_dict[len(word)].append(word)
+
+    def search(self, word):
+        if not word:
+            return False
+        if '.' not in word:
+            return word in self.word_dict[len(word)]
+        for v in self.word_dict[len(word)]:
+            # match xx.xx.x with yyyyyyy
+            for i, ch in enumerate(word):
+                if ch != v[i] and ch != '.':
+                    break
+            else:
+                return True
+        return False
+
+
+
+
 if __name__=="__main__":
     print(findCommon("This is document  tick  tick one one", "I get doc one is"))
     result = UncommonWords("Firstly this is the first string", "Next is the second string")
