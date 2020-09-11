219a76450adb1deb3bccfa6e3b335f0270003205
Nursultan Bekenov <nur.beken.usa@gmail.com>
2020-09-11 14:26:30 -0400

    add remove method

--- hash_tables/implement_linked_list.py
+++ hash_tables/implement_linked_list.py
@@ -9,8 +9,14 @@ class Node:
 
 class LinkedList:
 
-    def __init__(self):
+    def __init__(self, nodes=None):
         self.head = None
+        if nodes is not None:
+            node = Node(data=nodes.pop(0))
+            self.head = node
+            for elem in nodes:
+                node.next_node = Node(data=elem)
+                node = node.next_node
 
     def __repr__(self):
         node = self.head
@@ -28,21 +34,58 @@ class LinkedList:
         node = self.head
         while node is not None:
             yield node
-            node.next_node = node    
+            node = node.next_node    
 
-    def insert(self, newValue):
+    def insert(self, newNode):
         """
         add the new element to the head.
         """
-        newNode = Node(newValue)
         newNode.next_node = self.head
         self.head = newNode
     
-    def append_to_end(self, newValue):
-        newNode = Node(newValue)
+    def add_last(self, newNode):
         if not self.head:
             self.head = newNode
             return
         for cur_node in self:
             pass
-        cur_node.next_node = newNode    
+        cur_node.next_node = newNode
+
+    def add_after(self, target_node_data, newNode):
+        if not self.head:
+            raise Exception("The list is empty")
+        for cur_node in self:
+            if cur_node.value == target_node_data:
+                newNode.next_node = cur_node.next_node
+                cur_node.next_node = newNode
+                return
+
+        raise Exception("Node with data '%s' not found" % target_node_data)
+
+    def remove_node(self, target_node_data):
+        if not self.head:
+            raise Exception("The list is empty")
+        
+        # if wee need to remove head node
+        if self.head.value == target_node_data:
+            self.head = self.head.next_node
+            return
+
+        previous_node = self.head
+        for cur_node in self:
+            if cur_node.value == target_node_data:
+                previous_node.next_node = cur_node.next_node
+                return
+            previous_node = cur_node 
+
+        raise Exception("Node with data '%s' not found" % target_node_data)    
+
+
+
+if __name__=="__main__":
+    llist = LinkedList(["a", "b", "c", "d"])
+    llist.insert(Node("a1"))
+    llist.add_last(Node("f"))
+    print(llist)
+    llist.remove_node('r')
+    print(llist)
