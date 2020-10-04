2ccc3912727273762d4798aa71938ae6399f5198
Nursultan Bekenov <nur.beken.usa@gmail.com>
2020-10-03 21:07:45 -0400

    add is palnidrom

---
+++ linked_list/isPalindrome.py
@@ -0,0 +1,16 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def isPalindrome(self, head: ListNode) -> bool:
+        arr = []
+        
+        curr = head
+        
+        while curr is not None:
+            arr.append(curr.val)
+            curr = curr.next
+        return arr == arr[::-1]    
+            
