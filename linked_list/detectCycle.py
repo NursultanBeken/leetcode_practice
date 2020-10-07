b9839fcbbaf315e4ad4bd31209e0e9d859312759
Nursultan Bekenov <nur.beken.usa@gmail.com>
2020-10-06 20:19:37 -0400

    detect cycle

---
+++ linked_list/detectCycle.py
@@ -0,0 +1,20 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution:
+    def detectCycle(self, head: ListNode) -> ListNode:
+        curr = head
+        
+        visited = set()
+        
+        while curr:
+            if curr not in visited:
+                visited.add(curr)
+                curr = curr.next
+            else:
+                return curr
+        return None    
+        
