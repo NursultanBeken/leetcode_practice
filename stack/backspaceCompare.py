0415652ba352596ced28a3a154354fa6f8ae28e0
Nursultan Bekenov <nur.beken.usa@gmail.com>
2020-10-23 20:50:05 -0400

    feat: new task from leetcode

--- stack/backspaceCompare.py
+++ stack/backspaceCompare.py
@@ -18,3 +18,16 @@ class Solution:
                 
          
         return s_stack==t_stack
+
+
+class Solution(object):
+    def backspaceCompare(self, S, T):
+        def build(S):
+            ans = []
+            for c in S:
+                if c != '#':
+                    ans.append(c)
+                elif ans:
+                    ans.pop()
+            return "".join(ans)
+        return build(S) == build(T)
